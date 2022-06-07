6f558bec5ac61bf7b0eeebfcba3906502e06aab9
Ckichen <96504936+HelloChobo@users.noreply.github.com>
2022-06-07 17:08:38 +0900

    2022-06-07 first

---
+++ 01_java_data_type/src/number_and_string.java
@@ -0,0 +1,11 @@
+
+public class number_and_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+	}
+
+}
