9e14dc7e9c336c4218b05d71f555005a618c0f82
Ckichen <96504936+HelloChobo@users.noreply.github.com>
2022-06-11 21:36:56 +0900

    2022-06-11

--- java_programming/src/a_file_naming.java
+++ java_programming/src/a_file_naming.java
@@ -1,7 +1,7 @@
 
 public class a_file_naming {
 
-	public static void naming(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("출력입니다");
 	}
